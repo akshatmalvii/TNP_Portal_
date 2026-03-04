42ed3bc7584c807263a76e3d022fe836cbe31685
Hetvi <shahhetvi2404@gmail.com>
2026-03-04 12:39:30 +0530

    Initial Folder Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TNP Portal 🚀
+      </h1>
+    </div>
+ 
+    </>
+  )
+}
+
+export default App
