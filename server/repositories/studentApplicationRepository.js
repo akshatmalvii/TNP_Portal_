54bdc71f76f9582a884b19d1291e07f074b8ad6a
akshatmalvii <malviakshat2001@gmail.com>
2026-04-10 11:33:35 +0530

    Add status logic in notification and filter notification feature

--- server/repositories/studentApplicationRepository.js
+++ server/repositories/studentApplicationRepository.js
@@ -1,15 +1,35 @@
 import StudentApplication from "../models/student_application.js";
 import Drive from "../models/drive.js";
 import Company from "../models/company.js";
+import Offer from "../models/offer.js";
+import DriveRoundResult from "../models/drive_round_result.js";
+import DriveRound from "../models/drive_round.js";
 
 const findByStudent = async (student_id) => {
   return StudentApplication.findAll({
     where: { student_id },
-    include: [{
-      model: Drive,
-      attributes: ['drive_id', 'role_title', 'drive_status', 'package_lpa', 'deadline'],
-      include: [{ model: Company, attributes: ['company_name'] }]
-    }]
+    include: [
+      {
+        model: Drive,
+        attributes: ['drive_id', 'role_title', 'drive_status', 'package_lpa', 'deadline', 'offer_type'],
+        include: [{ model: Company, attributes: ['company_name'] }]
+      },
+      {
+        model: Offer,
+        required: false,
+      },
+      {
+        model: DriveRoundResult,
+        required: false,
+        include: [
+          {
+            model: DriveRound,
+            attributes: ['round_id', 'round_name', 'round_number', 'is_final_round'],
+          },
+        ],
+      },
+    ],
+    order: [['updated_at', 'DESC']]
   });
 };
 
